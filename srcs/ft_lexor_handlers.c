5e69b2a6cb767d9f66ea95259cdde31f336cc59f
dbanfi <banfidomen@gmail.com>
2022-01-24 20:46:08 +0100

    Fixed empty string in between special chars in lex #81

--- srcs/ft_lexor_handlers.c
+++ srcs/ft_lexor_handlers.c
@@ -12,7 +12,10 @@ void ft_lex_space_handler(char **current_str, char *args, int i, int begining)
 	if (FT_LEXOR_COMMENT)
 		printf("at 1-> begin: %d, i:%d, args: %s\n", begining, i , args);
 	*current_str = join2current_str(*current_str, ft_substr(args, begining, i - begining));
-	add_string(g_access.lexor2parser, *current_str);
+	if (ft_strlen(*current_str))
+		add_string(g_access.lexor2parser, *current_str);
+	free(*current_str);
+	*current_str = NULL;
 	free(*current_str);
 	*current_str = NULL;
 }
@@ -27,9 +30,10 @@ void ft_lex_space_handler(char **current_str, char *args, int i, int begining)
 void	ft_lex_operand_handler(char **current_str, char *args, int begining, int i)
 {
 	if (FT_LEXOR_COMMENT)
-			printf("at 2-> begin: %d, i:%d, args: %s\n", begining, i , args);
+		printf("at 2-> begin: %d, i:%d, args: %s\n", begining, i , args);
 	*current_str = join2current_str(*current_str, ft_substr(args, begining, i - begining));
-	add_string(g_access.lexor2parser, *current_str);
+	if (ft_strlen(*current_str))
+		add_string(g_access.lexor2parser, *current_str);
 	free(*current_str);
 	*current_str = NULL;
 	add_specialchar(g_access.lexor2parser, args[i]);
@@ -98,7 +102,8 @@ void ft_lex_string_reminder_handler(char **current_str, char *args, int begining
 	if (FT_LEXOR_COMMENT)
 		printf("at 5-> begin: %d, i:%d, args: %s\n", begining, i , args);
 	*current_str = join2current_str(*current_str, ft_substr(args, begining, i - begining));
-	add_string(g_access.lexor2parser, *current_str);
+	if (ft_strlen(*current_str))
+		add_string(g_access.lexor2parser, *current_str);
 	free(*current_str);
 	*current_str = NULL;
 }
