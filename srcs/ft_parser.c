3fdde5e503c806fbf7c9a24a1cecc8b00adac714
dbanfi <banfidomen@gmail.com>
2022-01-25 12:07:24 +0100

    Changed handling of specil cahrs in parser Fixed #81

--- srcs/ft_parser.c
+++ srcs/ft_parser.c
@@ -98,41 +98,49 @@ static char** add_to_line(char **line, char *new_str, int *line_len)
 	return new_line;
 }
 
-
 /*
 ** 	Handles errors
 */
 static void error_fun(t_list **list, t_list **lexor_list)
 {
+	if (FT_PARSER_COMMENT)
+		printf("!!!!!!!!!!!!!!!!!!!!!!!PARSER ERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 	ft_lstclear(list, ft_free_parser);
 	ft_free_lex_list(*lexor_list);
 	*lexor_list = NULL;
 }
 
-static char *create_char_str(char c, int len)
-{
-	int	counter;
-	char* str;
-
-	counter = 0;
-	str = (char *)ft_calloc(len + 1, sizeof(char));
-	while (counter < len)
-	{
-		str[counter] = c;
-		counter++;
-	}
-	return str;
-}
 
 static void ft_string_handler(t_list *lex_element, char	***cmd_line, int *cmd_len)
 {
 	char *str;
 
-	if (ft_strlen((char *)((t_word *)(lex_element->content))->address) != 0)
+	if (ft_strlen(((t_word *)(lex_element->content))->address) != 0)
 	{
-		str = ft_strdup((char *)((t_word *)(lex_element->content))->address);
+		str = ft_strdup(((t_word *)(lex_element->content))->address);
 		*cmd_line = add_to_line(*cmd_line, str, cmd_len);
 	}
+}
+
+/*
+**  @brief Chechk if string is same as one of redirects
+*/
+int is_redirect(char *str)
+{
+	if (!ft_strcmp(str, ">") || !ft_strcmp(str, "<") || !ft_strcmp(str, ">>") || !ft_strcmp(str, "<<"))
+		return (1);
+	return (0);
+}
+
+
+/*
+**  @brief Chechk if string is same as pip
+*/
+int is_pipe(char *str)
+{
+	if (!ft_strcmp(str, "|"))
+		return (1);
+	return (0);
 }
 
 int	parser(void)
@@ -140,7 +148,6 @@ int	parser(void)
 	int		index_counter;
 	t_list	*lex_element;
 	t_list	*executor_element;
-	char	first_redirect;
 	char	**cmd_line;
 	int		cmd_len;
 	char	*str;
@@ -149,7 +156,6 @@ int	parser(void)
 	index_counter = 0;
 	lex_element = *g_access.lexor2parser;
 	cmd = NULL;
-	first_redirect = 0;
 	while (1)
 	{
 		cmd_line = 0;
@@ -160,42 +166,27 @@ int	parser(void)
 		{	
 			if (lex_element == NULL)
 				break;
-			if (first_redirect != 0)
+			if (((t_word *)(lex_element->content))->type == FT_SPECIAL_CHAR_STRING)
 			{
-				if (((t_word *)(lex_element->content))->type == FT_STRING || *((char *)((t_word *)(lex_element->content))->address) != first_redirect)
-					str = create_char_str(first_redirect, 1);
-				else
+				if (is_redirect(((t_word *)(lex_element->content))->address))
 				{
-					str = create_char_str(first_redirect, 2);
-					lex_element = lex_element->next;
-				}
-				cmd_line = add_to_line(cmd_line, str, &cmd_len);
-				first_redirect = 0;
-			}
-			if (((t_word *)(lex_element->content))->type == FT_CHAR)
-			{
-				if (*((char *)((t_word *)(lex_element->content))->address) == FT_GREATER || *((char *)((t_word *)(lex_element->content))->address) == FT_LESSER || first_redirect != 0)
-				{
-					if (first_redirect == 0)
+					if (cmd_len == 0)
 					{
-						first_redirect = *((char *)((t_word *)(lex_element->content))->address);
-						if (cmd_len != 0)
-							break;
-						if (*((char *)((t_word *)((lex_element->next)->content))->address) == first_redirect)
-						{
-							str = create_char_str(first_redirect, 2);
-							lex_element = lex_element->next;
-						}
-						else
-							str = create_char_str(first_redirect, 1);
+						str = ft_strdup(((t_word *)(lex_element->content))->address);
+						cmd_line = add_to_line(cmd_line, str, &cmd_len);
 					}
-					cmd_line = add_to_line(cmd_line, str, &cmd_len);
-					first_redirect = 0;
+					else
+						break;
 				}
-				if (*((char *)((t_word *)(lex_element->content))->address) == FT_PIPE)
+				else  if (is_pipe(((t_word *)(lex_element->content))->address)) 
 				{
 					if (cmd_len == 0)
 						error_fun(g_access.parser2exec, g_access.lexor2parser);
+					break ;
+				}
+				else
+				{
+					error_fun(g_access.parser2exec, g_access.lexor2parser);
 					break ;
 				}
 			}
@@ -215,10 +206,6 @@ int	parser(void)
 		executor_element = ft_lstnew((void * ) cmd);
 		ft_lstadd_back(g_access.parser2exec, executor_element);
 		index_counter++;
-		if (lex_element == NULL)
-			break;
-		else
-			lex_element = lex_element->next;
 	}
 	ft_free_lex_list(*g_access.lexor2parser);
 	*g_access.lexor2parser = NULL;
