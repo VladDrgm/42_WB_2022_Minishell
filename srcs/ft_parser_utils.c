903d2eb8c0a2023801e8c1b8424c1617e5873f0b
dbanfi <banfidomen@gmail.com>
2022-02-01 15:25:03 +0100

    Added NULL terminator to the end of comm_tables Fixed #113

--- srcs/ft_parser_utils.c
+++ srcs/ft_parser_utils.c
@@ -20,7 +20,10 @@ static void	print_element_parser(void *input)
 	while(test_c < (cmd->comm_len))
 	{
 		// printf("1\n");
-		printf("%s\n", cmd->comm_table[test_c]);
+		if (cmd->comm_table[test_c] == NULL)
+			printf("0\n");
+		else
+			printf("%s\n", cmd->comm_table[test_c]);
 
 		// printf("2\n");
 		test_c++;
