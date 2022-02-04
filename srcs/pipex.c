b139023908ec67ef8fab0000b8d1265be101e690
dbanfi <banfidomen@gmail.com>
2022-02-04 10:44:48 +0100

    Connected pipes to parser Fixes #146

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -38,27 +38,27 @@ void make_cmd_list (t_list **cmd_list)
 	// temp->path = "/usr/bin/echo";
 	// ft_lstadd_back(cmd_list, ft_lstnew(temp));
 
-	temp = (t_command *)malloc(sizeof(t_command));
-	temp->comm_len = 3;
-	temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
-	temp->comm_table[0] = "cd";
-	temp->comm_table[1] = "..";
-	temp->comm_table[2] = NULL;
-	temp->index = 0;
-	temp->cmd_type = FT_CMD_TYPE_BUILT_IN;
-	temp->path = NULL;
-	ft_lstadd_back(cmd_list, ft_lstnew(temp));
+	// temp = (t_command *)malloc(sizeof(t_command));
+	// temp->comm_len = 3;
+	// temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
+	// temp->comm_table[0] = "cd";
+	// temp->comm_table[1] = "..";
+	// temp->comm_table[2] = NULL;
+	// temp->index = 0;
+	// temp->cmd_type = FT_CMD_TYPE_BUILT_IN;
+	// temp->path = NULL;
+	// ft_lstadd_back(cmd_list, ft_lstnew(temp));
 
-	temp = (t_command *)malloc(sizeof(t_command));
-	temp->comm_len = 3;
-	temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
-	temp->comm_table[0] = "cd";
-	temp->comm_table[1] = "-";
-	temp->comm_table[2] = NULL;
-	temp->index = 1;
-	temp->cmd_type = FT_CMD_TYPE_BUILT_IN;
-	temp->path = NULL;
-	ft_lstadd_back(cmd_list, ft_lstnew(temp));
+	// temp = (t_command *)malloc(sizeof(t_command));
+	// temp->comm_len = 3;
+	// temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
+	// temp->comm_table[0] = "cd";
+	// temp->comm_table[1] = "-";
+	// temp->comm_table[2] = NULL;
+	// temp->index = 1;
+	// temp->cmd_type = FT_CMD_TYPE_BUILT_IN;
+	// temp->path = NULL;
+	// ft_lstadd_back(cmd_list, ft_lstnew(temp));
 
 	// temp = (t_command *)malloc(sizeof(t_command));
 	// temp->comm_len = 3;
@@ -106,16 +106,38 @@ void make_cmd_list (t_list **cmd_list)
 	// temp->path= NULL;
 	// ft_lstadd_back(cmd_list, ft_lstnew(temp));
 
-	// temp = (t_command *)malloc(sizeof(t_command));
-	// temp->comm_len = 3;
-	// temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
-	// temp->comm_table[0] = "cat";
-	// temp->comm_table[1] = "-e";
-	// temp->comm_table[2] = NULL;
-	// temp->index = 1;
-	// temp->cmd_type = FT_CMD_TYPE_SYSTEM;
-	// temp->path = "/usr/bin/cat";
-	// ft_lstadd_back(cmd_list, ft_lstnew(temp));
+	temp = (t_command *)malloc(sizeof(t_command));
+	temp->comm_len = 3;
+	temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
+	temp->comm_table[0] = "cat";
+	temp->comm_table[1] = "-e";
+	temp->comm_table[2] = NULL;
+	temp->index = 0;
+	temp->cmd_type = FT_CMD_TYPE_SYSTEM;
+	temp->path = "/usr/bin/cat";
+	ft_lstadd_back(cmd_list, ft_lstnew(temp));
+
+	temp = (t_command *)malloc(sizeof(t_command));
+	temp->comm_len = 3;
+	temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
+	temp->comm_table[0] = "cat";
+	temp->comm_table[1] = "-e";
+	temp->comm_table[2] = NULL;
+	temp->index = 1;
+	temp->cmd_type = FT_CMD_TYPE_SYSTEM;
+	temp->path = "/usr/bin/cat";
+	ft_lstadd_back(cmd_list, ft_lstnew(temp));
+
+	temp = (t_command *)malloc(sizeof(t_command));
+	temp->comm_len = 3;
+	temp->comm_table = (char **)malloc(sizeof(char *) * temp->comm_len);
+	temp->comm_table[0] = "echo";
+	temp->comm_table[1] = "lala";
+	temp->comm_table[2] = NULL;
+	temp->index = 2;
+	temp->cmd_type = FT_CMD_TYPE_SYSTEM;
+	temp->path = "/usr/bin/echo";
+	ft_lstadd_back(cmd_list, ft_lstnew(temp));
 
 	// temp = (t_command *)malloc(sizeof(t_command));
 	// temp->comm_len = 2;
@@ -148,9 +170,9 @@ int	ft_super_mario(char **envp)
 	//ft_check_input_file(argv[1], argv[argc - 1]);
 	//ft_check_output_file(argv[argc - 1]);
 	//ft_make_cmd_list(argv, envp, argc, &cmd_list);
-	make_cmd_list(&cmd_list);
+	//make_cmd_list(&cmd_list);
 	//ft_pipex(cmd_list, envp);
-	test(cmd_list, envp);
+	test(g_access.parser2exec, envp);
 	return (1);
 }
 
