b30016c56390c9a639f2059dcfdc86f016a6bdc3
vdrag <vdragomir@protonmail.com>
2022-02-19 19:36:28 +0100

    Fixed Linux PATH getter. #211

--- srcs/ft_get_path_linux.c
+++ srcs/ft_get_path_linux.c
@@ -21,35 +21,24 @@ void ft_get_PATH(void)
 	}
 	fd = open("/etc/environment", O_RDONLY);
 	s = get_next_line(fd);
-	printf("s: %s\n", s);
 	while (s != NULL)
 	{
-		printf("s in while: %s\n", s);
 		if (!ft_strncmp(s, "PATH=", 5))
 		{
 			if (ptr != NULL)
 			{
-				printf("Test 1\n");
 				if (((t_env_var *)(ptr->content))->value != NULL)
 					free(((t_env_var *)(ptr->content))->value);
-				printf("Test 2\n");
 				((t_env_var *)(ptr->content))->value = ft_substr(s, 6, ft_strlen(s) - 6 - 1);
-				printf("((t_env_var *)(ptr->content))->value: %s\n", ((t_env_var *)(ptr->content))->value);
 			}
 			else
 			{
-				printf("Test 3\n");
 				env_var = (t_env_var *)malloc(sizeof(t_env_var));
 				env_var->name = ft_substr(s, 0, 5);
 				env_var->value = ft_substr(s, 6, ft_strlen(s) - 6 - 2);
-				printf("Test 4\n");
 				ft_lstadd_back(&(g_access.env), ft_lstnew(env_var));
-				printf("Test 5\n");
-				printf("((t_env_var *)(ptr->content))->name: %s\n", ((t_env_var *)(ptr->content))->name);
-				printf("((t_env_var *)(ptr->content))->value: %s\n", ((t_env_var *)(ptr->content))->value);
 			}
 		}
-		printf("Test 6\n");
 		if (s != NULL)
 		{
 			free(s);
